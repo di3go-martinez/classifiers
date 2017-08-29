90d3283875e2325eebce65b07a334eb9109bf135
Diego Ariel Martínez <diegoariel83@gmail.com>
2017-08-29 01:05:02 -0300

    examples

---
+++ rclassifiers/examples/create-classifier.jclassifier.sh
@@ -0,0 +1,2 @@
+curl --data "author=di3go&name=tp53solo&expressionAsJson=$(cat expression.applied-dataset.json)&groupsAsJson=$(cat groups.applied-dataset.json)&groupLabelsAsJson=$(cat groups-labels.json)" http://localhost:8080/functions
+
---
+++ rclassifiers/examples/evaluate-classifier.jclassifier.sh
@@ -0,0 +1,1 @@
+curl --data "mrna=$(cat mrna.json)" http://localhost:8080/functions/100
