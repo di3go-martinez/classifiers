e1568e90fb960c60a030d3043d2ba5b511e028cf
Diego Ariel Martínez <diegoariel83@gmail.com>
2017-08-28 22:38:07 -0300

    renombre de proyecto

--- deploy-to-docker-classifiers.sh
+++ deploy-to-docker-classifiers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
 #copio jar+deps y la db
-cp classifiers/build/libs/classifiers-0.0.1-SNAPSHOT.jar docker-classifiers/classifiers/classifiers.jar
-cp -r classifiers/db docker-classifiers/classifiers/
+cp jclassifiers/build/libs/classifiers-0.0.1-SNAPSHOT.jar docker-classifiers/classifiers/classifiers.jar
+cp -r jclassifiers/db docker-classifiers/classifiers/
 
 #armo el script R con todas las funciones necesarias
 cat rclassifiers/src/filters/* rclassifiers/src/functions/* > docker-classifiers/rclassifiers/main.r
