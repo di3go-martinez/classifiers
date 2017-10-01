62ae6e7c6c5c98ffc62c06b3cfe46a819e0fa8c5
bioplat <diegoariel83@gmail.com>
2017-10-01 17:49:21 -0300

    Comentario TODO para hacer persistente la base de dtaos

--- docker-classifiers/docker-run.sh
+++ docker-classifiers/docker-run.sh
@@ -1,3 +1,5 @@
+#TODO hacer base de datos persistente
+    #-v $( readlink -f classifiers/db/):/srv/classifiers/db/ \
 docker run --rm --detach \
     -p 8080:8080 \
     --name classifiers-service \
