32b16a3aeec7ff795a18dd06001dbf222cdec6c7
Diego Ariel Martínez <diegoariel83@gmail.com>
2017-10-01 23:51:05 -0300

    Fix transaccionalidad en los métodos

--- jclassifiers/src/main/java/org/bioplat/classifiers/controller/ClassifierFunctionsController.java
+++ jclassifiers/src/main/java/org/bioplat/classifiers/controller/ClassifierFunctionsController.java
@@ -9,9 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
+@Transactional
 public class ClassifierFunctionsController {
 
     private static final Logger logger = LoggerFactory.getLogger(ClassifierFunctionsController.class);
