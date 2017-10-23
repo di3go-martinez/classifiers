9a210b08b8454fc09a57c0e101f5d532772dc3d3
Diego Ariel Martínez <diegoariel83@gmail.com>
2017-10-22 21:46:46 -0300

    agrego verbose

--- rclassifiers/examples/evaluate-classifier.jclassifier.sh
+++ rclassifiers/examples/evaluate-classifier.jclassifier.sh
@@ -1,1 +1,1 @@
-curl --data "mrna=$(cat mrna.json)" http://localhost:8080/functions/${1?"falta el id del clasificador"}
+curl -v --data "mrna=$(cat mrna.json)" http://localhost:8080/functions/${1?"falta el id del clasificador"}
