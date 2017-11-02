0d79482556dc14420f5ffbdcb1f3b8f7e4f27eba
Diego Ariel Martínez <diegoariel83@gmail.com>
2017-11-02 00:49:38 -0300

    Construyo el cliente web, muevo el build al server y empaqueto todo

--- deploy-to-docker-classifiers.sh
+++ deploy-to-docker-classifiers.sh
@@ -1,9 +1,19 @@
 #!/bin/bash 
 
+
+(cd classifiers-web && npm run build)
+
+rm -rf jclassifiers/src/main/resources/static
+mv classifiers-web/build jclassifiers/src/main/resources/static
+
+(cd jclassifiers && ./gradlew build )
+
 #copio apps
 jar=$(ls jclassifiers/build/libs/jclassifiers-*.jar -tr | tail -n 1)
 echo "Deploying jclassifier $jar"
 cp $jar docker-classifiers/classifiers/classifiers.jar
+
+unzip -cq $jar META-INF/MANIFEST.MF | grep Version
 
 #armo el script R con todas las funciones necesarias
 cp rclassifiers/src/dependencies.r docker-classifiers/rclassifiers/dependencies.r
